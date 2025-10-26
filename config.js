e3eaa20e0216f41a2c3e31a467933d74f482c3f7
ccnucuc <140945117+ccnucuc@users.noreply.github.com>
2025-10-26 15:22:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
              lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "lavalink.jirayu.net", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 13592,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "youshallnotpass", 
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "nexus.voidhosting.vip", 
+                port: EnvironmentVariableProcessor.LAVALINK_PORT ||6004,       
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "cocaine", 
                 secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
             },
             
@@ -101,6 +101,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 
